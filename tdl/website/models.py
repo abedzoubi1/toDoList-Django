04216b98b80419e86c0fc9822638a91e74b97343
Abd Elrazek Zoubi <78377843+abedzoubi1@users.noreply.github.com>
2024-05-23 19:13:15 +0200

    Add files via upload

--- tdl/website/models.py
+++ tdl/website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ToDoItem(models.Model):
+    user = models.ForeignKey(User,null=True, on_delete= models.SET_NULL)
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete', 'due_date']
+    
+    
--- tdl/website/views.py
+++ tdl/website/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import ToDoItem
+
+from website.forms import SignUpForm
 # Create your views here.
 
 def home(request):
+    #get all items
+    items = ToDoItem.objects.all()
+    
     #check if user already logged in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,12 +25,34 @@ def home(request):
             messages.error(request, 'Faild to log in successfully!')
             return redirect('home')
     else:
-        return render(request, 'home.html')
+        return render(request, 'home.html', {'items': items})
     
 def logout_user(request):
     logout(request)
     return redirect('home')
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        #here we pass the post request because the already filled out the form
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,'You have been succesfully signed up!')
+            return redirect('home')
+    else:
+            #here we dont pass anything because they didnt fill out the form yet
+            form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
+
+            
+            
+            
+        
 
